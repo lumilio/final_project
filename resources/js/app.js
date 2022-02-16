fda57e501f4b615c9abe5b6507a6057206476d4c
pizzi9523 <pizzitolagiuseppe@live.it>
2022-02-16 10:46:48 +0100

    Validation unmatch password

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,4 +29,23 @@ Vue.component('advanced-search-component', require('./components/AdvancedCompone
 
 const app = new Vue({
     el: '#app',
+    data: {
+
+    },
+
+    mounted() {
+        var password = document.getElementById("password")
+            , confirm_password = document.getElementById("password-confirm");
+
+        function validatePassword() {
+            if (password.value != confirm_password.value) {
+                confirm_password.setCustomValidity("Passwords Don't Match");
+            } else {
+                confirm_password.setCustomValidity('');
+            }
+        }
+
+        password.onchange = validatePassword;
+        confirm_password.onkeyup = validatePassword;
+    }
 });
