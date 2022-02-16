b4c35ec1afe26341b5dde868b3f8f2ab82a29d8d
lumilio <logistic-division@lumilium.com>
2022-02-16 14:51:39 +0100

    fix form for service e adress

--- database/migrations/2022_02_11_172417_create_apartments_table.php
+++ database/migrations/2022_02_11_172417_create_apartments_table.php
@@ -15,7 +15,7 @@ class CreateApartmentsTable extends Migration
     {
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
-            $table->string('address')->unique();
+            $table->string('address');
             $table->text('description')->nullable();
             $table->string('title');
             $table->string('slug')->unique();
