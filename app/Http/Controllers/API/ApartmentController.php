b0e5d08c9f5168aced44e9011bfc8af2f0ab384a
Andretreccia <andreabtreccia@gmail.com>
2022-02-22 16:53:48 +0100

    add condition for the null value and improves search condition

--- app/Http/Controllers/API/ApartmentController.php
+++ app/Http/Controllers/API/ApartmentController.php
@@ -32,11 +32,25 @@ class ApartmentController extends Controller
 
         //Alternative  Giuseppe method
 
-        $requestQuery = $request->query();
+        //$requestQuery = $request->query();
         $reqServices = explode(',', $request->services);
         $latitude = $request->latitude;
         $longitude = $request->longitude;
-        $distance = $request->distance;
+        //$distance = $request->distance;
+
+        if ($request->distance < 20) {
+            $distance = 20;
+        } else
+            $distance = $request->distance;
+        if ($request->n_bed < 1) {
+            $n_bed = 1;
+        } else
+            $n_bed = $request->n_bed;
+        if ($request->n_room < 1) {
+            $n_rooms = 1;
+        } else
+            $n_rooms = $request->n_rooms;
+
 
 
         /* funzione per calcolare la distanza */
@@ -44,14 +58,14 @@ class ApartmentController extends Controller
 
 
         //$aparts = Apartment::all();
-        $aparts = Apartment::where('n_bed', '>', $requestQuery['n_bed'])
-            ->where('n_rooms', '>', $requestQuery['n_rooms'])
+        $aparts = Apartment::where('n_bed', '>=', $n_bed)
+            ->where('n_rooms', '>=', $n_rooms)
             ->get();
 
 
         if (!empty($request->services)) {
-            $aparts = Apartment::where('n_bed', '>', $requestQuery['n_bed'])
-                ->where('n_rooms', '>', $requestQuery['n_rooms'])
+            $aparts = Apartment::where('n_bed', '>=', $n_bed)
+                ->where('n_rooms', '>=', $n_rooms)
                 ->whereHas('services', function ($param) use ($reqServices) {
                     $param->WhereIn('service_id', $reqServices);
                 })->get();
