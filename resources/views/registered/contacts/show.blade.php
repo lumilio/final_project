769fe9894463e5536a1ee2ac79af75c04d703b1e
pizzi9523 <pizzitolagiuseppe@live.it>
2022-02-23 12:15:51 +0100

    add bold to message detail

--- resources/views/registered/contacts/show.blade.php
+++ resources/views/registered/contacts/show.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="container mt-5">
         <p>Messaggio inviato da {{ $contact->email }}, {{ $contact->name }} </p>
-        <p>Oggetto: {{ $contact->oggetto_mail }}</p>
-        <p>Messaggio: <br>
+        <p> <span class="fw-bold">Oggetto:</span> {{ $contact->oggetto_mail }}</p>
+        <p><span class="fw-bold">Messaggio:</span> <br>
             {{ $contact->message }}
         </p>
-        <p>Data: {{ $contact->created_at }} </p>
+        <p><span class="fw-bold">Data:</span> {{ $contact->created_at }} </p>
     </div>
 @endsection
