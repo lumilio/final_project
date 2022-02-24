f9655ac207f54cb3dc475105b552d6c853e1ae87
Alessia9802 <alessiamisciagna@yahoo.it>
2022-02-24 12:20:29 +0100

    Fix layout Advanced Search page

--- resources/js/components/AdvancedSearch.vue
+++ resources/js/components/AdvancedSearch.vue
@@ -1,106 +1,121 @@
 <template>
     <div class="container font_style">
-        <div class="row justify-content-center">
+        <div class="row justify-content-left">
             <!-- Filter Rooms and Kilometers -->
-            <div class="row justify-content-center">
-                <div
-                    class="d-flex flex-wrap justify-content-center align-items-center"
-                >
-                    <div class="col-6 col-sm-3 mx-2 mb-3 fields">
-                        <label for="n_rooms" class="form-label">
-                            N° camere Min</label
-                        >
-                        <input
-                            type="number"
-                            min="0"
-                            max="200"
-                            class="form-control"
-                            name="n_rooms"
-                            id="n_rooms"
-                            aria-describedby="n_roomsHelper"
-                            placeholder="0"
-                            v-model="n_rooms"
-                        />
-                    </div>
-                    <div class="col-6 col-sm-3 mx-2 mb-3 fields">
-                        <label for="n_bed" class="form-label"
-                            >N° letti Min</label
-                        >
-                        <input
-                            type="number"
-                            min="0"
-                            max="5000"
-                            class="form-control"
-                            name="n_bed"
-                            id="n_bed"
-                            aria-describedby="n_bedHelper"
-                            placeholder="0"
-                            v-model="n_bed"
-                        />
-                    </div>
-                    <div class="col-6 col-sm-3 mx-2 mb-3 fields">
-                        <label for="distance" class="form-label"
-                            >Raggio distanza Km</label
-                        >
-                        <input
-                            type="number"
-                            min="20"
-                            max="5000"
-                            class="form-control"
-                            name="distance"
-                            id="distance"
-                            aria-describedby="distanceHelper"
-                            placeholder="0"
-                            v-model="distance"
-                        />
-                    </div>
-                </div>
-
-                <!--Services filter -->
-                <div
-                    class="d-flex service_checkbox flex-wrap justify-content-center align-items-center"
-                >
+            <div class="col-md-6">
+                <div class="row justify-content-left">
                     <div
-                        class="py-2"
-                        v-for="service in services"
-                        v-bind:key="service.id"
+                        class="d-flex flex-wrap justify-content-left align-items-center"
                     >
-                        <div class="form-check mx-2">
+                        <div class="col-6 col-sm-3 mx-2 mb-3 fields">
+                            <label for="n_rooms" class="form-label">
+                                N° camere Min</label
+                            >
                             <input
-                                type="checkbox"
-                                class="form-check-input"
-                                :name="service.name"
-                                :id="service.id"
-                                :value="service.id"
-                                v-model="v_services"
+                                type="number"
+                                min="0"
+                                max="200"
+                                class="form-control"
+                                name="n_rooms"
+                                id="n_rooms"
+                                aria-describedby="n_roomsHelper"
+                                placeholder="1"
+                                v-model="n_rooms"
                             />
-                            <label class="form-check-label service_name" for="">
-                                {{ service.name }}
-                            </label>
+                        </div>
+                        <div class="col-6 col-sm-3 mx-2 mb-3 fields">
+                            <label for="n_bed" class="form-label"
+                                >N° letti Min</label
+                            >
+                            <input
+                                type="number"
+                                min="0"
+                                max="5000"
+                                class="form-control"
+                                name="n_bed"
+                                id="n_bed"
+                                aria-describedby="n_bedHelper"
+                                placeholder="1"
+                                v-model="n_bed"
+                            />
+                        </div>
+                        <div class="col-6 col-sm-3 mx-2 mb-3 fields">
+                            <label for="distance" class="form-label"
+                                >Raggio distanza Km</label
+                            >
+                            <input
+                                type="number"
+                                min="20"
+                                max="5000"
+                                class="form-control"
+                                name="distance"
+                                id="distance"
+                                aria-describedby="distanceHelper"
+                                placeholder="20"
+                                v-model="distance"
+                            />
+                        </div>
+                    </div>
+                    <!--Services filter -->
+                    <div
+                        class="d-flex service_checkbox flex-wrap justify-content-left align-items-center"
+                    >
+                        <div
+                            class="py-2"
+                            v-for="service in services"
+                            v-bind:key="service.id"
+                        >
+                            <div class="form-check mx-2">
+                                <input
+                                    type="checkbox"
+                                    class="form-check-input"
+                                    :name="service.name"
+                                    :id="service.id"
+                                    :value="service.id"
+                                    v-model="v_services"
+                                />
+                                <label
+                                    class="form-check-label service_name"
+                                    for=""
+                                >
+                                    {{ service.name }}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Button Advanced Search -->
+                    <div class="col-md-6">
+                        <div
+                            class="d-flex mx-1 justify-content-left align-items-center"
+                        >
+                            <button
+                                class="btn btn_search text-white my-3"
+                                @click="searchFunction()"
+                            >
+                                Ricerca avanzata
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- Button Advanced Search -->
-            <div class="col-6">
-                <div class="d-flex justify-content-center align-items-center">
-                    <button
-                        class="btn btn_search text-white my-3"
-                        @click="searchFunction()"
-                    >
-                        Ricerca avanzata
-                    </button>
+            <!-- Mappa -->
+            <div class="col-md-6">
+                <div
+                    class="d-flex justify-content-center align-items-center map bg-dark"
+                >
+                    <p class="text-white text-center fs-4">Mappa</p>
                 </div>
             </div>
         </div>
+
         <!-- Results Advanced search -->
         <div class="row mt-4">
             <div
-                class="col-md-6 d-flex justify-content-center align-items-center"
+                class="d-flex flex-wrap justify-content-center align-items-center"
             >
                 <a
                     :href="'/guest/apartments/' + apartment.slug"
-                    class="card justify-content-between card_adv m-3"
+                    class="card justify-content-between card_promo m-3"
                     v-for="apartment in apartments"
                     :key="apartment.id"
                 >
@@ -122,15 +137,6 @@
                         <span>View details</span>
                     </div>
                 </a>
-            </div>
-
-            <!-- Mappa -->
-            <div class="col-md-6">
-                <div
-                    class="d-flex justify-content-center align-items-center my-4 map bg-dark"
-                >
-                    <p class="text-white text-center fs-4">Mappa</p>
-                </div>
             </div>
         </div>
     </div>
