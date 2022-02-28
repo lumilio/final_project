7b4517e497caa66f8c71fe95300f5ec2d7c3f2e2
Andretreccia <andreabtreccia@gmail.com>
2022-02-28 18:12:27 +0100

    fix cards and responsive

--- resources/js/components/AdvancedSearch.vue
+++ resources/js/components/AdvancedSearch.vue
@@ -1,251 +1,247 @@
 <template>
-  <div class="container font_style">
-    <div class="row justify-content-left">
-      <!-- Filter Rooms and Kilometers -->
-      <div class="col-md-6">
+    <div class="container font_style">
         <div class="row justify-content-left">
-          <div class="d-flex flex-wrap justify-content-left align-items-center">
-            <div class="col-6 col-sm-3 mx-2 mb-3 fields">
-              <label for="n_rooms" class="form-label"> N° camere Min</label>
-              <input
-                type="number"
-                min="0"
-                max="200"
-                class="form-control"
-                name="n_rooms"
-                id="n_rooms"
-                aria-describedby="n_roomsHelper"
-                placeholder="1"
-                v-model="n_rooms"
-              />
+            <!-- Filter Rooms and Kilometers -->
+            <div class="col-md-6">
+                <div class="row justify-content-left">
+                    <div
+                        class="d-flex flex-wrap justify-content-left align-items-center"
+                    >
+                        <div class="col-6 col-sm-3 mx-2 mb-3 fields">
+                            <label for="n_rooms" class="form-label">
+                                N° camere Min</label
+                            >
+                            <input
+                                type="number"
+                                min="0"
+                                max="200"
+                                class="form-control"
+                                name="n_rooms"
+                                id="n_rooms"
+                                aria-describedby="n_roomsHelper"
+                                placeholder="1"
+                                v-model="n_rooms"
+                            />
+                        </div>
+                        <div class="col-6 col-sm-3 mx-2 mb-3 fields">
+                            <label for="n_bed" class="form-label"
+                                >N° letti Min</label
+                            >
+                            <input
+                                type="number"
+                                min="0"
+                                max="5000"
+                                class="form-control"
+                                name="n_bed"
+                                id="n_bed"
+                                aria-describedby="n_bedHelper"
+                                placeholder="1"
+                                v-model="n_bed"
+                            />
+                        </div>
+                        <div class="col-6 col-sm-3 mx-2 mb-3 fields">
+                            <label for="distance" class="form-label"
+                                >Raggio distanza Km</label
+                            >
+                            <input
+                                type="number"
+                                min="20"
+                                max="5000"
+                                class="form-control"
+                                name="distance"
+                                id="distance"
+                                aria-describedby="distanceHelper"
+                                placeholder="20"
+                                v-model="distance"
+                            />
+                        </div>
+                    </div>
+                    <!--Services filter -->
+                    <div
+                        class="d-flex service_checkbox flex-wrap justify-content-left align-items-center"
+                    >
+                        <div
+                            class="py-2"
+                            v-for="service in services"
+                            v-bind:key="service.id"
+                        >
+                            <div class="form-check mx-2">
+                                <input
+                                    type="checkbox"
+                                    class="form-check-input"
+                                    :name="service.name"
+                                    :id="service.id"
+                                    :value="service.id"
+                                    v-model="v_services"
+                                />
+                                <label
+                                    class="form-check-label service_name"
+                                    for=""
+                                >
+                                    {{ service.name }}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Button Advanced Search -->
+                    <div class="col-md-6">
+                        <div
+                            class="d-flex mx-1 justify-content-left align-items-center"
+                        >
+                            <button
+                                class="btn btn_search text-white my-3"
+                                @click="searchFunction()"
+                            >
+                                Ricerca avanzata
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-6 col-sm-3 mx-2 mb-3 fields">
-              <label for="n_bed" class="form-label">N° letti Min</label>
-              <input
-                type="number"
-                min="0"
-                max="5000"
-                class="form-control"
-                name="n_bed"
-                id="n_bed"
-                aria-describedby="n_bedHelper"
-                placeholder="1"
-                v-model="n_bed"
-              />
+            <!-- Mappa -->
+            <div class="col-md-6">
+                <div class="d-flex justify-content-center align-items-center">
+                    <div id="map" ref="mapRef" class="col-4 col-4-md"></div>
+                </div>
             </div>
-            <div class="col-6 col-sm-3 mx-2 mb-3 fields">
-              <label for="distance" class="form-label"
-                >Raggio distanza Km</label
-              >
-              <input
-                type="number"
-                min="20"
-                max="5000"
-                class="form-control"
-                name="distance"
-                id="distance"
-                aria-describedby="distanceHelper"
-                placeholder="20"
-                v-model="distance"
-              />
-            </div>
-          </div>
-          <!--Services filter -->
-          <div
-            class="
-              d-flex
-              service_checkbox
-              flex-wrap
-              justify-content-left
-              align-items-center
-            "
-          >
+        </div>
+
+        <!-- Results Advanced search -->
+        <div
+            class="row flex-wrap justify-content-center align-items-center mt-4"
+        >
             <div
-              class="py-2"
-              v-for="service in services"
-              v-bind:key="service.id"
+                class="col-12 col-sm-6 col-md-4 col-xl-3"
+                v-for="apartment in apartments"
+                :key="apartment.id"
             >
-              <div class="form-check mx-2">
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  :name="service.name"
-                  :id="service.id"
-                  :value="service.id"
-                  v-model="v_services"
-                />
-                <label class="form-check-label service_name" for="">
-                  {{ service.name }}
-                </label>
-              </div>
+                <div class="ads card card_promo m-3">
+                    <a :href="'/guest/apartments/' + apartment.slug" class="">
+                        <img
+                            class="card-img-top thumb"
+                            :src="'storage/' + apartment.image"
+                            alt="Card image cap"
+                        />
+                    </a>
+
+                    <h2 class="card-text m-3 card_title">
+                        {{ apartment.title }}
+                    </h2>
+                    <div class="box">
+                        <p class="card-text m-3">{{ apartment.description }}</p>
+                    </div>
+
+                    <div
+                        class="button_details p-2 w-50 justify-content-center align-items-center text-center text-white m-auto mt-4 mb-4"
+                    >
+                        <span
+                            ><a :href="'/guest/apartments/' + apartment.slug"
+                                >View details</a
+                            ></span
+                        >
+                    </div>
+                </div>
             </div>
-          </div>
-          <!-- Button Advanced Search -->
-          <div class="col-md-6">
-            <div class="d-flex mx-1 justify-content-left align-items-center">
-              <button
-                class="btn btn_search text-white my-3"
-                @click="searchFunction()"
-              >
-                Ricerca avanzata
-              </button>
-            </div>
-          </div>
         </div>
-      </div>
-      <!-- Mappa -->
-      <div class="col-md-6">
-        <div class="d-flex justify-content-center align-items-center">
-          <div id="map" ref="mapRef" class="col-4 col-4-md"></div>
-        </div>
-      </div>
     </div>
-
-    <!-- Results Advanced search -->
-    <div class="row flex-wrap justify-content-center align-items-center mt-4">
-      <div
-        class="col-12 col-sm-12 col-md-6 col-xl-4"
-        v-for="apartment in apartments"
-        :key="apartment.id"
-      >
-        <div class="card justify-content-between card_promo m-3">
-          <a :href="'/guest/apartments/' + apartment.slug" class="">
-            <img
-              class="card-img-top thumb"
-              :src="'storage/' + apartment.image"
-              alt="Card image cap"
-            />
-          </a>
-
-          <p class="promo">Promotion</p>
-          <h2 class="card-text m-3 card_title">
-            {{ apartment.title }}
-          </h2>
-          <div class="box">
-            <p class="card-text m-3">{{ apartment.description }}</p>
-          </div>
-
-          <div
-            class="
-              button_details
-              p-2
-              w-50
-              justify-content-center
-              align-items-center
-              text-center text-white
-              m-auto
-              mt-4
-              mb-4
-            "
-          >
-            <span
-              ><a :href="'/guest/apartments/' + apartment.slug"
-                >View details</a
-              ></span
-            >
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
 import { Bus } from "../app";
 export default {
-  data() {
-    return {
-      map: null,
-      apikey: "Oe8qW7UX2GW9LFGSM2ePZNH5D3IpOBqK",
-      apartments: [],
-      userInput: "",
-      n_bed: "",
-      n_rooms: "",
-      v_services: [],
-      distance: null,
-      coordinates: {},
-    };
-  },
-  props: {
-    services: Array,
-    show_route: String,
-  },
-  methods: {
-    searchFunction() {
-      // console.log(this.userInput);
-      axios
-        .get(
-          `/api/apartments?n_rooms=${this.n_rooms}&n_bed=${this.n_bed}&services=${this.v_services}&latitude=${this.coordinates.lat}&longitude=${this.coordinates.lon}&distance=${this.distance}`
-        )
-        .then((response) => {
-          this.apartments = response.data.data;
-          this.createMap(
-            this.apartments[0].longitude,
-            this.apartments[0].latitude
-          );
-
-          this.addMarker(this.map, this.apartments);
-
-          console.log(this.apartments);
-          // console.log(this.n_rooms);
-          // console.log(this.n_rooms);
-        })
-        .catch((error) => {
-          console.log(error, "non funziona");
-        });
+    data() {
+        return {
+            map: null,
+            apikey: "Oe8qW7UX2GW9LFGSM2ePZNH5D3IpOBqK",
+            apartments: [],
+            userInput: "",
+            n_bed: "",
+            n_rooms: "",
+            v_services: [],
+            distance: null,
+            coordinates: {},
+        };
     },
-    serviceFunction() {
-      // console.log(this.coordinates);
+    props: {
+        services: Array,
+        show_route: String,
     },
+    methods: {
+        searchFunction() {
+            // console.log(this.userInput);
+            axios
+                .get(
+                    `/api/apartments?n_rooms=${this.n_rooms}&n_bed=${this.n_bed}&services=${this.v_services}&latitude=${this.coordinates.lat}&longitude=${this.coordinates.lon}&distance=${this.distance}`
+                )
+                .then((response) => {
+                    this.apartments = response.data.data;
+                    this.createMap(
+                        this.apartments[0].longitude,
+                        this.apartments[0].latitude
+                    );
 
-    createMap(lon, lat) {
-      const tt = window.tt;
-      this.map = tt.map({
-        key: this.apikey,
-        container: "map",
-        center: [lon, lat], // Madrid
-        zoom: 9,
-        theme: {
-          style: "buildings",
-          layer: "basic",
-          source: "vector",
+                    this.addMarker(this.map, this.apartments);
+
+                    console.log(this.apartments);
+                    // console.log(this.n_rooms);
+                    // console.log(this.n_rooms);
+                })
+                .catch((error) => {
+                    console.log(error, "non funziona");
+                });
         },
-      });
-      this.map.addControl(new tt.FullscreenControl());
-      this.map.addControl(new tt.NavigationControl());
+        serviceFunction() {
+            // console.log(this.coordinates);
+        },
+
+        createMap(lon, lat) {
+            const tt = window.tt;
+            this.map = tt.map({
+                key: this.apikey,
+                container: "map",
+                center: [lon, lat], // Madrid
+                zoom: 9,
+                theme: {
+                    style: "buildings",
+                    layer: "basic",
+                    source: "vector",
+                },
+            });
+            this.map.addControl(new tt.FullscreenControl());
+            this.map.addControl(new tt.NavigationControl());
+        },
+        addMarker(map, apartments) {
+            const tt = window.tt;
+            console.log(apartments);
+            apartments.forEach((apartment) => {
+                var location = [apartment.longitude, apartment.latitude];
+                var popupOffset = 25;
+
+                var marker = new tt.Marker().setLngLat(location).addTo(map);
+                var popup = new tt.Popup({ offset: popupOffset });
+                marker.setPopup(popup).togglePopup();
+                // console.log(popup);
+                // console.log(marker._element);
+            });
+        },
     },
-    addMarker(map, apartments) {
-      const tt = window.tt;
-      console.log(apartments);
-      apartments.forEach((apartment) => {
-        var location = [apartment.longitude, apartment.latitude];
-        var popupOffset = 25;
 
-        var marker = new tt.Marker().setLngLat(location).addTo(map);
-        var popup = new tt.Popup({ offset: popupOffset });
-        marker.setPopup(popup).togglePopup();
-        // console.log(popup);
-        // console.log(marker._element);
-      });
+    created() {
+        Bus.$on("sendCoordinates", (data) => {
+            this.coordinates = data;
+        });
+        // console.log(this.coordinates);
+        if (localStorage.coordinates) {
+            this.coordinates = JSON.parse(localStorage.getItem("coordinates"));
+            // console.log(this.coordinates);
+        }
     },
-  },
 
-  created() {
-    Bus.$on("sendCoordinates", (data) => {
-      this.coordinates = data;
-    });
-    // console.log(this.coordinates);
-    if (localStorage.coordinates) {
-      this.coordinates = JSON.parse(localStorage.getItem("coordinates"));
-      // console.log(this.coordinates);
-    }
-  },
-
-  mounted() {
-    this.searchFunction();
-    // this.createMap();
-    // this.addMarker(this.map);
-  },
+    mounted() {
+        this.searchFunction();
+        // this.createMap();
+        // this.addMarker(this.map);
+    },
 };
 /*appunti prima di iniziare
 
